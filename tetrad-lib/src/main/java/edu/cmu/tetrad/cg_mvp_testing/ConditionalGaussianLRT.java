60aaef7805f072cd9800871c608b7c9faeb76af8
Joseph Ramsey <jdramsey@andrew.cmu.edu>
2017-07-28 04:55:16 -0400

    added files for cg and mvp tests

---
+++ tetrad-lib/src/main/java/edu/cmu/tetrad/cg_mvp_testing/ConditionalGaussianLRT.java
@@ -0,0 +1,50 @@
+package edu.cmu.tetrad.algcomparison.independence;
+
+import edu.cmu.tetrad.data.DataModel;
+import edu.cmu.tetrad.data.DataUtils;
+import edu.cmu.tetrad.util.Parameters;
+import edu.cmu.tetrad.data.DataType;
+import edu.cmu.tetrad.search.IndTestConditionalGaussianLRT;
+import edu.cmu.tetrad.search.IndependenceTest;
+import edu.cmu.tetrad.util.Experimental;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Wrapper for Fisher Z test.
+ *
+ * @author jdramsey
+ */
+public class ConditionalGaussianLRT implements IndependenceWrapper, Experimental {
+    static final long serialVersionUID = 23L;
+
+    @Override
+    public IndependenceTest getTest(DataModel dataSet, Parameters parameters) {
+        final IndTestConditionalGaussianLRT test
+                = new IndTestConditionalGaussianLRT(DataUtils.getMixedDataSet(dataSet),
+                parameters.getDouble("alpha"),
+                parameters.getInt("discretize") > 0);
+        test.setNumCategoriesToDiscretize(parameters.getInt("numCategoriesToDiscretize"));
+        return test;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Conditional Gaussian Likelihood Ratio Test";
+    }
+
+    @Override
+    public DataType getDataType() {
+        return DataType.Mixed;
+    }
+
+    @Override
+    public List<String> getParameters() {
+        List<String> parameters = new ArrayList<>();
+        parameters.add("alpha");
+        parameters.add("discretize");
+        return parameters;
+    }
+
+}
